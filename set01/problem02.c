4009d70cf97265c7bb39b730e417aec459c2ecf5
mohithrvu <119392357+mohithrvu@users.noreply.github.com>
2022-12-15 21:41:32 +0530

    Update problem02.c

---
+++ set01/problem02.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int a ;
+int b ;
+int sum ;
+
+int main() {
+ printf("Enter a number: ");
+ scanf("%d", &a);
+ printf("Enter a number: ");
+ scanf("%d", &b);
+ sum = a + b;
+ printf("%d",sum);
+  
+}
