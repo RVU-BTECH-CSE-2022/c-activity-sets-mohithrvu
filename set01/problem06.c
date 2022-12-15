310a3212942cd063ac6dc579837c0043e84dce6e
mohithrvu <119392357+mohithrvu@users.noreply.github.com>
2022-12-15 21:53:22 +0530

    Update problem06.c

---
+++ set01/problem06.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+int main()
+{
+  int a,b,c,largest;
+  a=input(&a);
+  b=input(&b);
+  c=input(&c);
+  largest=compare(a,b,c,largest);
+  output(a,b,c,largest);
+}
+int input()
+{
+  int n;
+  printf("Enter a number");
+  scanf("%d", &n);
+    return n;
+}
+int compare(int a,int b, int c){
+int largest;
+
+if((a>b)&&(a>c))
+{
+  *largest=a;
+}
+else if((b>c)&&(c>a))
+  {
+  largest=b;
+  }
+else
+  if((*c>*a)&&(*a>*b))
+  {
+  *largest=c;
+  }
+return largest;}
+
+output(int a,int b,int c, int largest)
+{
+  printf("The largest among %d %d and %d is %d",a,b,c,largest);
+}
