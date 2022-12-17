757de2a5bec4116284fbeb0a407c55d191eee543
mohithrvu <119392357+mohithrvu@users.noreply.github.com>
2022-12-17 23:41:16 +0530

    Update problem08.c

--- set01/problem08.c
+++ set01/problem08.c
@@ -1,23 +1,45 @@
 #include<stdio.h>
-int main()
+int input_array_size();
+ void input_array(int n,int a[n]);
+  int sum_n_array(int a[],int n,int total);
+  void output(int total);
+int main(void)
 {
-  int x,y;
-  x=input();
-  y=natural_sum(x);
-  output(x,y);
+  int n,sum,total;
+  n=input_array_size(n);
+  int a[n];
+  input_array(n,a);
+  total=sum_n_array(a,n,total);
+  output(total);
+  return 0;
 }
-int input()
-{
+int input_array_size(){
   int n;
-  printf("Enter a number");
+  printf("Enter arrazy size : ");
   scanf("%d", &n);
   return n;
 }
-int natural_sum(int n,int sum)
+void input_array(int n,int a[n])
 {
-  sum=(n*(n+1))/2;
-  return sum;
+int i=0;
+for(i=0;i<n;i++)
+  {
+printf("Enter array num: ");
+scanf("%d", &a[i]);}
 }
-void output(int x,int y){
-  printf("The sum of natural numbers %d is %d",x,y);
+int sum_n_array(int a[],int n,int total)
+{
+  //sum= 0;
+  //int total;
+  total=0;
+ int i=0;
+  for(i=0;i<n;i++){
+    total= total+a[i];
+    printf("%d\n", total);
+  }
+  return total;
 }
+void output(int total)
+{
+  printf("The sum of numbers is:  %d", total);
+  }
